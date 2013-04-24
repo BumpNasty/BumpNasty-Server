35cd18f3360e81e9fbc80385a90376066b588ca8
Justin Halsall <justin@juice10.com>
2013-04-24 12:24:15 +0200

    add configurable port

--- app.js
+++ app.js
@@ -11,5 +11,7 @@ app.get('/hello.json', function(req, res){
 });
 
 
-app.listen(80);
-console.log('Listening on port 80');
+var port = process.env.PORT || 80;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
