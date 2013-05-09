840daab90cc45be05a133e5c006198bd72ce68ca
Justin Halsall <justin@juice10.com>
2013-05-09 12:40:11 +0200

    add configurable api keys that aren\'t set in github

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 BumpNasty-Server
 ================
+
+Copy config/ean-example.js to config/ean.js and insert expedia api key into config/ean.js
---
+++ config/ean-example.js
@@ -0,0 +1,6 @@
+// copy this file to config/ean.js
+// fill in the api key below (for more info ask Tim)
+module.exports = {
+  apiKey : 'APIKEYHERE',
+  cid : '55505'
+}
